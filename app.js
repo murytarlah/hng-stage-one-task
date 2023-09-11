58c8fe1d021d8ab582e027f1c946938f7c4a03e0
murytarlah <murytarlah.dev@gmail.com>
2023-09-11 16:00:50 +0100

    feat: added the api get request and also adjusted port value

--- app.js
+++ app.js
@@ -8,8 +8,32 @@ dotenv.config();
 const port = process.env.PORT
 
 
-// Define a route that handles GET requests to '/api'
 
+// Define a route that handles GET requests to '/api'
+app.get('/api', (req, res) => {
+    // Get query parameters from the request
+    const slackName = req.query.slack_name;
+    const track = req.query.track;
+
+    // Get the current date and time
+    const currentDate = new Date();
+    const currentDay = currentDate.toLocaleDateString('en-US', { weekday: 'long' });
+    const utcTime = currentDate.toISOString();
+
+    // Create the response JSON object
+    const response = {
+        slack_name: slackName,
+        current_day: currentDay,
+        utc_time: utcTime,
+        track: track,
+        github_file_url: 'https://github.com/murytarlah/hng-stage-one-task/master/app.js',
+        github_repo_url: 'https://github.com/murytarlah/hng-stage-one-task',
+        status_code: 200,
+    };
+
+    // Send the JSON response
+    res.json(response);
+});
 
 // Start the Express server
 app.listen(port, () => {
